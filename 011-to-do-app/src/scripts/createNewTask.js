b323aa8a67a8a789b2e2aa0f526b58103795cfd1
José Victor <jose.vdr.pessoal@gmail.com>
2025-04-21 01:04:49 -0300

    Adds a new task to a project

--- 011-to-do-app/src/scripts/createNewTask.js
+++ 011-to-do-app/src/scripts/createNewTask.js
@@ -2,20 +2,17 @@ import { renderProjects } from "./renderProjects";
 
 export default function createNewTask(event) {
     const button = event.target;
-    const parentDiv = button.closest('.project');
+    const project = button.closest('.project');
     const form = document.querySelector('.task-form');
-
-    const id = parentDiv.classList.item(1);
-    const taskStringfied = localStorage.getItem(id);
-    const task = JSON.parse(taskStringfied);
-    
-    task.tasks.push(form.elements['task-desc'].value);
-    
+    const id = project.classList.item(1);
+    const task = JSON.parse(localStorage.getItem(id));
     const submitTask = document.querySelector('#submit-task');
 
     submitTask.addEventListener('click', ()=> {
+        const newTask = form.elements['task-desc'].value;
+        task.tasks.push(newTask);
         localStorage.setItem(id, JSON.stringify(task));
         renderProjects();
     });
-   
+
 }
