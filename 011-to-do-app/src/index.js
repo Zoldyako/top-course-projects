9632b392c699944d271759db9f4ba374b7960a95
José Victor <jose.vdr.pessoal@gmail.com>
2025-04-21 07:34:17 -0300

    UPDATED modal buttons to close and clear form

--- 011-to-do-app/src/index.js
+++ 011-to-do-app/src/index.js
@@ -1,42 +1,48 @@
-import './styles/normalize.css';
-import './styles/index.css';
-import './styles/buttons.css';
+import "./styles/normalize.css";
+import "./styles/index.css";
+import "./styles/buttons.css";
 import { setDefaultProjects } from "./scripts/setDefaultProjects.js";
 import { renderProjects } from "./scripts/renderProjects.js";
 import { createNewProject } from "./scripts/createNewProject.js";
 
+const projectModal = document.querySelector(".new-project-btn");
+const taskModal = document.querySelector(".new-task-btn");
+const submitProject = document.querySelector("#submit-project");
+const newProjectBtn = document.querySelector(".new-project");
+const deleteAll = document.querySelector(".clear-projects");
+const closeModalProjectBtn = document.querySelector("#close-modal-project");
+const closeModalTaskBtn = document.querySelector("#close-modal-task");
+const projectForm = document.querySelector(".project-form");
+const taskForm = document.querySelector(".task-form");
+const defaultProjectBtn = document.querySelector(".default-projects");
 
-const modal = document.querySelector('.new-project-btn');
-const submitProject = document.querySelector('#submit-project');
-const newProjectBtn = document.querySelector('.new-project');
-const deleteAll = document.querySelector('.clear-projects');
-const closeModalBtn = document.querySelector('#close-modal');
-const projectForm = document.querySelector('.project-form');
-const defaultProjectBtn = document.querySelector('.default-projects');
-
-
-defaultProjectBtn.addEventListener('click', () => {
+defaultProjectBtn.addEventListener("click", () => {
   setDefaultProjects();
   renderProjects();
 });
 
-newProjectBtn.addEventListener('click', () => {
+newProjectBtn.addEventListener("click", () => {
   modal.showModal();
 });
 
-deleteAll.addEventListener('click', () => {
+deleteAll.addEventListener("click", () => {
   localStorage.clear();
   renderProjects();
 });
 
-submitProject.addEventListener('click', () => {
+submitProject.addEventListener("click", () => {
   createNewProject();
   renderProjects();
 });
 
-closeModalBtn.addEventListener('click', () => {
+closeModalProjectBtn.addEventListener("click", () => {
   projectForm.reset();
-  modal.close();
+  projectModal.close();
+});
+
+closeModalTaskBtn.addEventListener("click", () => {
+  taskForm.reset();
+  taskModal.close();
 });
 
 renderProjects();
