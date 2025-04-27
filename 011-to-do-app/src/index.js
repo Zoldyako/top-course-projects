323ad8b3195a16000a9df93a20f98b1ed012cf11
José Victor <jose.vdr.pessoal@gmail.com>
2025-04-27 17:50:34 -0300

    Update index.js with delet function

--- 011-to-do-app/src/index.js
+++ 011-to-do-app/src/index.js
@@ -4,5 +4,14 @@ import "./styles/sidebar.css";
 import "./styles/projects.css";
 import "./styles/buttons.css";
 import { render } from "./scripts/render.js";
+import { deletProjects, createDefaultProjects } from "./scripts/projectsUtils.js"
 
+// createDefaultProjects();
 render();
+
+const deletBtn = document.querySelector('.delet-projects');
+deletBtn.addEventListener('click', () => {
+    deletProjects();
+    render();
+});
+
