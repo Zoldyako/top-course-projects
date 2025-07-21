732eec402f661bb88a71896a272811ccc85778ae
José Victor Dias Rodrigues <jose.vdr.pessoal@gmail.com>
2025-07-21 06:08:03 -0300

    UPDATES main.js with basic tests

--- 015-hash-map/src/main.ts
+++ 015-hash-map/src/main.ts
@@ -1,3 +1,18 @@
 import HashMap from "./HashMap.js";
 
 const list = HashMap();
+
+
+list.set('José', 'Zoldyako');
+list.set('Zoldyako', 'My artistic name');
+list.set('ABC', 'ABC');
+console.log(`Getting: 'José': ${list.get('José')}`)
+console.log(`Getting: 'Zoldyako': ${list.get('Zoldyako')} | 'ABC':  ${list.get('ABC')}`)
+
+console.log(`\nUpdating 'José': ${list.set('José', 'Victor')}`);
+console.log(`Getting 'José': ${list.get('José')}`);
+
+console.log(`\nDoes it has 'José': ${list.has('José')} | 'Zoldyako': '${list.has('Zoldyako')} | ABC': ${list.has('ABC')} | 'Victor': ${list.has('Victor')}`)
+
+console.log(`\nRemoving 'ABC': ${list.remove('ABC')}`);
+console.log(`Has 'ABC': ${list.has('ABC')}`);
